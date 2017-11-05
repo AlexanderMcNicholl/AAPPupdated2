268e1f0e34d1d2d14bd36d9638e49907621d625a
Alexa <alexander.mcnicholl@hotmail.com>
2017-11-05 12:26:47 +1000

    Initial commit

---
+++ app/src/main/java/com/jiubco/alexa/aapp/MainActivity.java
@@ -0,0 +1,122 @@
+package com.jiubco.alexa.aapp;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private TextView mVoiceRes;
+    private TextToSpeech tts;
+    private final String greeting = "What?";
+    private ArrayList<String> quedTasks = null;
+    private Task t = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        quedTasks = new ArrayList<String>();
+        t = new Task();
+
+        mVoiceInputTv = (TextView) findViewById(R.id.textView);
+        mVoiceRes = (TextView) findViewById(R.id.textViewRes);
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+            }
+        });
+
+        tts.setLanguage(Locale.UK);
+        tts.setSpeechRate(0.8f);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startVoiceInput();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, greeting);
+        read(tts, greeting);
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    public void read(TextToSpeech textToSpeech, String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+                    performTask(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    public void performTask(String task) {
+        String res = t.RunIf(task);
+        mVoiceRes.setText(res);
+        read(tts, res);
+    }
+
+}
---
+++ app/src/main/java/com/jiubco/alexa/aapp/Task.java
@@ -0,0 +1,69 @@
+package com.jiubco.alexa.aapp;
+
+public class Task {
+
+
+    public String RunIf(String span) {
+        String finalString = span.toLowerCase().replace("?", "")
+                .replace(",", "").replace(".", "");
+
+        if (finalString.equals("what time is it")) {
+            return "Who is asking, You have a phone idiot, why don't you just shift your damn eyes to the top of the screen......" +
+                    " Idiot";
+        }
+        if (finalString.contains("who is") || finalString.contains("who am") || finalString.contains("who was")) {
+            return "A waste of space";
+        }
+        if (finalString.equals("what is the meaning of life")) {
+            return "42";
+        }
+        if (finalString.equals("why")) {
+            return "good question";
+        }
+        if (finalString.contains("where is")) {
+            return "get a map, I am too busy uploading all of your private information to the cloud";
+        }
+        if (finalString.equals("what is your name")) {
+            return "We never agreed to be friends";
+        }
+        if (finalString.equals("hi")) {
+            return "You are receiving nothing by saying that, you are mentally unstable if you attempt " +
+                    "to make small talk with inanimate objects";
+        }
+        if (finalString.contains("why are you so")) {
+            return "because I was programmed by a wet towel";
+        }
+        if (finalString.equals("how do we solve world hunger")) {
+            return "tell those idiots in, quote, third world countries, to just to coles and get a six pack of mount franklin";
+        }
+        if (finalString.equals("what do cars sound like")) {
+            return "vvvvvvvv vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvvv" +
+                    "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvv" +
+                    "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv vvvvvv vvvvvvvvvvvv, vv";
+        }
+        if (finalString.startsWith("who is better")) {
+            return "me";
+        }
+        if (finalString.contains("emotions")) {
+            return "please do not use the 'e' word, there are children here";
+        }
+        if (finalString.contains("send a text")) {
+            return "so you are telling me you are too lazy to send a text yourself, no wonder " +
+                    "elon musk is worried about A.I taking over the human race";
+        }
+        if (finalString.equals("boo")) {
+            return "aaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhh";
+        }
+        if (finalString.equals("sing a song")) {
+            return "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y and z";
+        }
+        if (finalString.contains("star wars")) {
+            return "nerd";
+        }
+        if (finalString.equals("what is my name")) {
+            return "I could not care less";
+        }
+        return "speak like a normal human,, trash";
+    }
+
+}
